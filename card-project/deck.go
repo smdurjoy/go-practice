05a92e34659f5c5195903fca99a052f745bf86d4
smdurjoy <smdurjoy963@gmail.com>
2022-07-18 22:28:09 +0600

    new deck func

--- card-project/deck.go
+++ card-project/deck.go
@@ -4,8 +4,23 @@ import "fmt"
 
 type deck [] string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"One", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+ 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- card-project/main.go
+++ card-project/main.go
@@ -1,12 +1,7 @@
 package main
 
 func main() {
-	cards := deck{"One card", "Two card", newCard()}
-	cards = append(cards, "Hello card")
+	cards := newDeck()
 	
-	cards.print();
-}
-
-func newCard() string {
-	return "New Card"
+	cards.print()
 }
